385d89b20ec522086fe219e72a8c8cd47004d957
Eyal Levy <eyal@levys.co.il>
2026-05-12 11:42:22 +0300

    add soanr integration

--- vars/codeQuality.groovy
+++ vars/codeQuality.groovy
@@ -1,3 +1,38 @@
 def sonarqubeScan(){
     echo "Scan with Sonarqube"
 } 
+
+def sonarLocalScan() {
+    withSonarQubeEnv('SonarQube') {
+
+        sh """
+            sonar-scanner \
+              -Dsonar.projectKey=${env.JOB_NAME} \
+              -Dsonar.projectName=${env.JOB_NAME} \
+              -Dsonar.sources=. \
+              -Dsonar.sourceEncoding=UTF-8
+        """
+    }
+}
+
+
+def sonarLocalScanWithGate() {
+    withSonarQubeEnv('SonarQube') {
+
+        sh """
+            sonar-scanner \
+              -Dsonar.projectKey=${env.JOB_NAME} \
+              -Dsonar.projectName=${env.JOB_NAME} \
+              -Dsonar.sources=. \
+              -Dsonar.sourceEncoding=UTF-8
+        """
+    }
+
+    timeout(time: 5, unit: 'MINUTES') {
+        def qualityGate = waitForQualityGate()
+
+        if (qualityGate.status != 'OK') {
+            error "SonarQube Quality Gate failed: ${qualityGate.status}"
+        }
+    }
+}
